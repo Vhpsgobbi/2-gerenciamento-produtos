015262ee28577db22d49b76d321ca6da460134d0
VictorSgobbi <victorhugosgobbi@gmail.com>
2026-03-23 23:44:14 -0300

    Criação do service ProdutoService

---
+++ src/main/java/com/example/demo/service/ProdutoService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Produto;
+import com.example.demo.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Produto criar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Optional<Produto> buscarPorId(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        produtoRepository.deleteById(id);
+    }
+}
